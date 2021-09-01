10d277e1eff263d0c533823b08509894d4e89739
duong <tdbui1209@gmail.com>
2021-09-02 06:36:46 +0700

    remove 'take data' function

--- hand.py
+++ hand.py
@@ -203,30 +203,6 @@ class Hand:
             rescaled.append(distance / total)
         return rescaled
 
-
-def data():
-    capture = cv2.VideoCapture(0)
-    hand_detector = Hand(max_num_hands=1)
-    count = 0
-    while True:
-        _, frame = capture.read()
-        frame = hand_detector.detect(frame)
-        cv2.imshow('Detector', frame)
-        cv2.waitKey(1)
-        try:
-            with open('test.txt', encoding='utf-8', mode='a') as file:
-                for i in hand_detector.data:
-                    if i == hand_detector.data[-1]:
-                        file.write(str(i))
-                    else:
-                        file.write(str(i) + ',')
-                count += 1
-                file.write('\n')
-        except:
-            continue
-        print(count)
-
-
 def main():
     filename = 'euclid_model.sav'
     capture = cv2.VideoCapture(0)
